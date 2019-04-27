f7cd8dc6f392d848451740b4e2535fd6dd78ac87
Samyak Gaur <samyakgaur@yahoo.com>
2019-04-27 10:01:52 +0530

    Create solution.py

---
+++ Division/solution.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    #floor division it divides and rounds down the value
+    print(a//b)
+    #normal division 
+    print(a/b)
