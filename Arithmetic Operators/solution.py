f71c6d8c78fa5d0d4ffb881a56f0fea69daba16c
Samyak Gaur <samyakgaur@yahoo.com>
2019-04-27 09:59:34 +0530

    Create solution.py

---
+++ Arithmetic Operators/solution.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    //automatically goes to the next line as it sees print stmt
+    print(a+b)
+    print(a-b)
+    print(a*b)
